7e14841f1876954ad79cf332ee33fce436093fc6
Brice Christian <bricechristian93@gmail.com>
2022-01-09 00:35:30 -0500

    Updated flickitity styling on mobile

--- src/scripts/components/flickity.js
+++ src/scripts/components/flickity.js
@@ -1,1 +1,15 @@
 import Flickity from "flickity-imagesloaded";
+
+var tapArea, startX ;
+tapArea = document.querySelectorAll('.carousel');
+startX = 0;
+for (var item of tapArea) {
+    item.ontouchstart = function(e) {
+        startX = e.touches[0].clientX;
+    };
+    item.ontouchmove = function(e) {
+        if (Math.abs(e.touches[0].clientX - startX) > 5 && e.cancelable ) {
+            e.preventDefault();
+        }
+    };
+}
