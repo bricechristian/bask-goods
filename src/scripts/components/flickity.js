f3b1091430b2829949adaa2d337479572895d733
Brice Christian <bricechristian93@gmail.com>
2022-02-01 00:00:52 -0500

    Updated flickity scroll bug on mobile

--- src/scripts/components/flickity.js
+++ src/scripts/components/flickity.js
@@ -4,12 +4,14 @@ var tapArea, startX ;
 tapArea = document.querySelectorAll('.carousel');
 startX = 0;
 for (var item of tapArea) {
-    item.ontouchstart = function(e) {
-        startX = e.touches[0].clientX;
-    };
-    item.ontouchmove = function(e) {
-        if (Math.abs(e.touches[0].clientX - startX) > 5 && e.cancelable ) {
-            e.preventDefault();
-        }
-    };
+    if(!item.classList.contains("md:overflow-auto")){
+        item.ontouchstart = function(e) {
+            startX = e.touches[0].clientX;
+        };
+        item.ontouchmove = function(e) {
+            if (Math.abs(e.touches[0].clientX - startX) > 5 && e.cancelable ) {
+                e.preventDefault();
+            }
+        };
+    }
 }
